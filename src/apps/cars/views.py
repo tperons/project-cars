54a75f6c57ffb3095238dae04bccc6d832ae086c
tperons <tperons@gmail.com>
2025-09-22 20:37:32 -0300

    feat(core): Associa o usuário ao cadastro de novos registros
    Implementa a funcionalidade para que, ao criar um novo registro, o usuário autenticado seja automaticamente definido como o autor/criador do mesmo.

--- src/apps/cars/views.py
+++ src/apps/cars/views.py
@@ -98,6 +98,13 @@ class CarCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     success_message = "O veículo %(brand)s %(model)s foi cadastrado com sucesso!"
     login_url = reverse_lazy('users:login')
 
+    def form_valid(self, form):
+        car = form.save(commit=False)
+        car.owner = self.request.user
+        car.save()
+        form.save_m2m()
+        return super().form_valid(form)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update({
